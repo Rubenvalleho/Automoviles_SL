c3ff2dd427a8bbc44ff82775b63ba3ac8b964a79
Rubenvaljar <ruben.valjar@educa.jcyl.es>
2023-11-09 16:00:46 +0100

    Vehiculo 1 añadido al main

--- src/main.java
+++ src/main.java
@@ -1,3 +1,15 @@
 public class main {
+    public static void main(String[] args) {
+        Vehiculos vehiculo1 = new Vehiculos();
+        vehiculo1.setMarca("Dacia");
+        vehiculo1.setModelo("Sandero");
+        vehiculo1.setColor("Gris");
+        vehiculo1.setCombustible("Diesel");
+        vehiculo1.setMotor("1.5 DCI");
+        vehiculo1.setCv("95 cv");
+        vehiculo1.setMatricula("7777hrf");
 
+        Vehiculos vehiculo2 = new Vehiculos();
+        vehiculo1.
+    }
 }
